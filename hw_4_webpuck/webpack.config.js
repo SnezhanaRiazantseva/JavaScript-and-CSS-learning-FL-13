b5ca582f3e4f2339cca93bec00952d0357047e1b
MonsiSome <62303105+MonsiSome@users.noreply.github.com>
2020-07-10 13:56:47 +0300

    Finish setups

--- hw_4_webpuck/webpack.config.js
+++ hw_4_webpuck/webpack.config.js
@@ -1,42 +1,110 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin') // Require  html-webpack-plugin plugin
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const path = require('path')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // Require  html-webpack-plugin plugin
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, '/src/js/index.js'), // webpack entry point. Module to start building dependency graph
+  entry: './src/js/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'), // Folder to store generated bundle
+    path: path.join(__dirname, './dist'), // Folder to store generated bundle
     filename: 'js/app.js', // Name of generated bundle after build
-    publicPath: '/dist' // public URL of the output directory when referenced in a browser
+    publicPath: '/dist/', // public URL of the output directory when referenced in a browser
+  },
+  optimization: {
+    minimizer: [
+      new OptimizeCssAssetsWebpackPlugin(),
+      new TerserPlugin(),
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        inject: 'body',
+        minify: {
+          removeAttributeQuotes: true,
+          collapseWhitespace: true,
+          removeComments: true,
+        },
+      }),
+    ],
   },
   module: {
     // where we defined file patterns and their loaders
-    rules: [{
-      test: /\.js$/,
-      use: 'babel-loader',
-      exclude: [/node_modules/]
-    },
-    {
-      test: /\.(sass|scss)$/,
-      use: ExtractTextPlugin.extract({
-        fallback: 'style-loader',
-        use: ['css-loader', 'sass-loader']
-      })
-    }
-    ]
-
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        use: [{ loader: 'babel-loader' }, { loader: 'eslint-loader' }],
+      },
+      {
+        test: /\.(sass|scss)$/,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader'],
+      },
+      {
+        test: /\.(jpg|png|svg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[hash].[ext]',
+              outputPath: './img',
+            },
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65,
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.9],
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75,
+              },
+            },
+          },
+        ],
+      },
+    ],
   },
   plugins: [
     // Array of plugins to apply to build chunk
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, '/src/index.html'),
-      inject: 'body'
+    // new HtmlWebpackPlugin({
+    //   template: './src/index.html',
+    //   inject: 'body',
+    // }),
+    new MiniCssExtractPlugin({
+      filename: 'styles/styles.css',
+      inject: 'head',
     }),
-    new ExtractTextPlugin('styles/styles.css')
+    new CleanWebpackPlugin({
+      dry: true,
+    }),
   ],
   devServer: {
     // configuration for webpack-dev-server
-    contentBase: './src/index.html', // source of static assets
-    port: 7700 // port to run dev-server
-  }
-}
+    open: 'Google Chrome',
+    contentBase: './dist/', // source of static assets
+    port: 7700, // port to run dev-server
+    hot: true,
+  },
+};

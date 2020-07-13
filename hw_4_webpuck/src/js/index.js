791108a9c1c71f525edd8ba2c479e88f4f6a4265
MonsiSome <62303105+MonsiSome@users.noreply.github.com>
2020-07-13 15:33:15 +0300

    Add initial html&styles

--- hw_4_webpuck/src/js/index.js
+++ hw_4_webpuck/src/js/index.js
@@ -1,10 +1,43 @@
 import '../scss/styles.scss';
 import '../scss/typography.scss';
 
-class TestClass {
-  constructor() {
-    const msg = 'Using ES2015+ syntax';
-    console.log(msg);
+let isGameStarted = false;
+// import { AlertService } from "./app/alert.service";
+// import { ComponentService } from "./app/component.service";
+// const alertService = new AlertService();
+// const componentService = new ComponentService();
+// run(alertService, componentService);
+// class TestClass {
+//   constructor() {
+//     const msg = 'Using ES2015+ syntax';
+//     console.log(msg);
+//   }
+// }
+// const test = new TestClass();
+let personChoices = [];
+let randomChoices = [];
+
+function run(event) {
+  event.preventDefault();
+  if (event.target.tagName !== 'BUTTON') {
+    return;
   }
+  if (personChoices.length >= 3) {
+    endTheGame();
+  }
+  if (!isGameStarted) {
+    isGameStarted = true;
+  }
+  // let personChoices = [];
+  // let randomChoices = [];
+  personChoices.push(event.target.className);
+
+  console.log(event.target.className);
 }
-const test = new TestClass();
+
+let buttons = document.getElementsByClassName('button-wrapper')[0];
+buttons.addEventListener('click', run);
+
+function endTheGame() {
+  isGameStarted = false;
+}

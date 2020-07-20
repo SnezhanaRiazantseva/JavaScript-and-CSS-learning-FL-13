f29b8acd99769c551d2b88d1a0e77a5859d87258
MonsiSome <62303105+MonsiSome@users.noreply.github.com>
2020-07-20 13:57:15 +0300

    Add courses data model

--- FL_13_05_homework_react_redux/src/index.js
+++ FL_13_05_homework_react_redux/src/index.js
@@ -3,5 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+const COURSES = [
+  { date: "18.02.18", name: "Prerequisites", description: "Webpack, AngularCLI, TypeScript.", duration: "1:34" },
+  { date: "01.02.18", name: "Components", description: "Components; lifecicle, templete DSL and data-binding, Custom component.", duration: "2:10" },
+  { date: "15.01.18", name: "Directives + Pipes", description: "Directives, types pf directives, built-in directives, custom directive, pipes, built-in pipes, custom pipes, async pipe...", duration: "1:34" },
+  { date: "28.12.17", name: "Modules & Services", description: "Services, DI, modules, lazy Loading.", duration: "10:00" },
+  {date: '', name: '', description: '', duration: ''},
+];
+
 ReactDOM.render(<App />, document.getElementById('root'));
 

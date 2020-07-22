c15e99dda89d8c9499673fe7abe5929f2857c95d
MonsiSome <62303105+MonsiSome@users.noreply.github.com>
2020-07-22 16:58:58 +0300

    add class OptionsDropdown and styles to CoursePage

--- FL_13_05_homework_react_redux/src/App.js
+++ FL_13_05_homework_react_redux/src/App.js
@@ -2,12 +2,29 @@ import React from 'react';
 import './App.css';
 
 const COURSES = [
-  { date: "18.02.18", name: "Prerequisites", description: "Webpack, AngularCLI, TypeScript.", duration: "1:34" },
+  { date: "18.02.18", name: "Prerequisites", description: "Webpack, Angular CLI, TypeScript.", duration: "1:34" },
   { date: "01.02.18", name: "Components", description: "Components; lifecicle, templete DSL and data-binding, Custom component.", duration: "2:10" },
   { date: "15.01.18", name: "Directives + Pipes", description: "Directives, types pf directives, built-in directives, custom directive, pipes, built-in pipes, custom pipes, async pipe...", duration: "1:34" },
   { date: "28.12.17", name: "Modules & Services", description: "Services, DI, modules, lazy Loading.", duration: "10:00" },
   // {date: '', name: '', description: '', duration: ''},
 ];
+
+class OptionsDropdown extends React.Component {
+  render() {
+    return(
+      <div className='dropdown'>
+        <p>
+          <img src='../edit.svg' alt='Edit icon' />
+          Edit
+        </p>
+        <p>
+          <img src='../delete.svg' alt='Delete icon'/>
+          Delete
+        </p>
+      </div>
+    )
+  }
+}
 
 class CoursesRow extends React.Component {
   render() {
@@ -17,15 +34,21 @@ class CoursesRow extends React.Component {
     if (durationFormat[0].length === 1) {
       durationFormat[0] = '0' + durationFormat[0];
     }
-    let duration = durationFormat.join(':');
+    durationFormat[0] = durationFormat[0] + 'h';
+    durationFormat[1] = durationFormat[1] + 'min';
+    let duration = durationFormat.join(' ');
 
     return (
-      <tr>
-        <td>{courses.date}</td>
-        <td>{courses.name}</td>
-        <td>{courses.description}</td>
-        <td>{duration}</td>
-      </tr>
+      <div className="courseRow">
+        <div>{courses.date}</div>
+        <div className="courseName">{courses.name}</div>
+        <div>{courses.description}</div>
+        <div>{duration}</div>
+        <div className="datacellDropdown"> 
+          <div className="menuDots">&#183; &#183; &#183;</div>
+          <OptionsDropdown/> 
+        </div>
+      </div>
     )
   }
 }
@@ -41,18 +64,48 @@ class CoursesTable extends React.Component {
       )
     })
     return (
-      <table>
-        <tbody>{rows}</tbody>
-      </table>
+      <div className="coursesTable">
+        {rows}
+      </div>
+    )
+  }
+}
+
+class SearchBar extends React.Component {
+  render() {
+    return(
+      <input className="searchBar" type="search" placeholder="&#128269; &#8195;Search" />
+    )
+  }
+}
+
+class Button extends React.Component {
+  render() {
+    return(
+      <button className="button">Add course</button>
+    )
+  }
+}
+
+class CoursesPage extends React.Component {
+  render() {
+    return(
+      <div className="coursesPage">
+        <div className="barWrapper">
+          <SearchBar/>
+          <Button/>
+        </div>
+        <CoursesTable courses={COURSES}/>
+      </div>
     )
   }
 }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">Learn React!</header>
-      <CoursesTable courses={COURSES}/>
+    <div className="App"> 
+    {/* delete this div? */}
+      <CoursesPage/>
     </div>
     );
   }
